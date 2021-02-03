2eedd9e1239f90f840d825875f015bfdb1ebc34f
amedoc <ahmed.mahmoudi.mk@gmail.com>
2021-02-03 20:27:00 +0100

    commenting the main program test and adding an test routine for RRAM under sw/ to see the behaviour of TLM transactions

---
+++ sw/RRAM_test/main.cpp
@@ -0,0 +1,56 @@
+#include <string.h>
+#include <iostream>
+
+
+static char* const RRAM_START_ADDR = reinterpret_cast<char* const>(0x03000000);
+//static const unsigned int RRAM_SIZE = 0x00001FF;
+//static volatile uint32_t * const RRAM_START_ADDR = (uint32_t * const)0x03000000;
+
+
+
+int main() {
+
+
+	uint16_t buffer[256]; // having 2 bytes for each value
+	for (int i=0;i<=255;i++)
+		{
+			buffer[i]=0x0001;
+			std::cout << "initial value of buffer ["<< i<<"] is "<<buffer[i]<< std::endl;
+		}
+
+
+
+	// copying values from RRAM to buffer should 0 as RRAM is initialized to NULL
+	memcpy(buffer, RRAM_START_ADDR,256);
+	for (int i=0;i<=255;i++)
+		{
+		std::cout << "value of buffer ["<< i<<"] copied from RRAM is "<<buffer[i]<< std::endl;
+		}
+
+	// Putting 0xFFFF in all buffer elements
+	for (int i=0;i<=255;i++)
+		{
+		buffer[i] = 0xFFFF;
+		}
+
+	// copying values from buffer to RRAM
+	memcpy(RRAM_START_ADDR, buffer,256);
+
+	// reset te values in buffer
+	for (int i=0;i<=255;i++)
+		{
+		buffer[i] = 0x0000;
+		}
+
+	// copying values from RRAM to buffer should be multiplied
+	memcpy(buffer, RRAM_START_ADDR,256);
+	for (int i=0;i<=255;i++)
+		{
+		std::cout << "value of buffer ["<< i<<"] copied from RRAM is "<<buffer[i]<< std::endl;
+		}
+
+	return 0;
+}
+
+
+
